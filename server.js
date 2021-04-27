f097e6eb04513705602d5e4f8ec21f4fc7800d37
ejsmith13 <ejsmithwork@gmail.com>
2021-04-26 20:55:23 -0400

    add routes to server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const routes = require("./routes");
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -14,6 +15,8 @@ if (process.env.NODE_ENV === "production") {
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/rcitylocations");
 
