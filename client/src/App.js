94d44f7efacac1d5afae6a5abf5e548627ab9319
ejsmith13 <ejsmithwork@gmail.com>
2021-05-03 20:55:38 -0400

    added landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,31 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
 import Wrapper from "./components/Wrapper";
-import Footer from './components/Footer'
-import NewLocation from './pages/createNew'
-import List from './pages/listView'
-import './App.css';
-import MapView from './pages/mapView';
-import Home from './pages/home'
+import Footer from "./components/Footer";
+import NewLocation from "./pages/createNew";
+import List from "./pages/listView";
+import "./App.css";
+import MapView from "./pages/mapView";
+import Home from "./pages/home";
+import Landing from "./pages/landing";
 
 function App() {
   return (
     <Router>
-    <div className="App pageContainer">
-      <div className= "contentWrap">
-     <Navbar />
-     <Wrapper>
-       <Route exact path = {["/", "/home"]} component={Home} />
-     <Route exact path = "/add" component={NewLocation} />
-     <Route exact path = "/list" component={List} />
-     <Route exact path = "/map" component ={MapView} />
-     </Wrapper>
-     </div>
-     <Footer className="footer"/>
-     
-    </div>
+      <div className="App pageContainer">
+        <div className="contentWrap">
+          <Navbar />
+          <Wrapper>
+            <Route exact path="/landing" component={Landing} />
+            <Route exact path={["/", "/home"]} component={Home} />
+            <Route exact path="/add" component={NewLocation} />
+            <Route exact path="/list" component={List} />
+            <Route exact path="/map" component={MapView} />
+          </Wrapper>
+        </div>
+        <Footer className="footer" />
+      </div>
     </Router>
   );
 }
---
+++ client/src/pages/landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Landing =()=> {
+    return(
+        <div>
+            <h1>this is the landing page</h1>
+        </div>
+    )
+}
+
+export default Landing;
