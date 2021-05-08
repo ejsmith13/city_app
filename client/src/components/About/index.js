2fdf25a657b39e066ad1cbc7163e989063817cb9
ejsmith13 <ejsmithwork@gmail.com>
2021-05-08 16:35:33 -0400

    changed home screen layout

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './style.css';
+
 import {Button} from 'react-bootstrap'
 
 function About () {
     return(
-        <div className= "container home-container">
-            <h1>R_City</h1>
+        <div className= "col-md-8 about-container">
+            <h1>RCity</h1>
             <p>Welcome to RCity, the app that lets you explore everything there is to do in Richmond, VA.
                 Whether you are looking for somewhere to take a new date, go out on a Friday night, or enjoy
                 a lazy Sunday afternoon, we have what you are looking for. Places are added by locals to showcase
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,29 +1,29 @@
-import React, {useState, useEffect} from 'react';
-import About from '../components/About'
-import Carousels from '../components/Carousel'
-import API from "../utils/API"
+import React, { useState, useEffect } from "react";
+import About from "../components/About";
+import Carousels from "../components/Carousel";
+import API from "../utils/API";
 
-function Home () {
-    const [places, setPlaces] = useState([])
+function Home() {
+  const [places, setPlaces] = useState([]);
 
-    useEffect(() => {
-        loadPlaces()
-      }, [])
+  useEffect(() => {
+    loadPlaces();
+  }, []);
 
-    function loadPlaces (){
-        API.getLocation()
-      .then(res => 
-        setPlaces(res.data)
-      )
-      .catch(err => console.log(err));
-    }
-    return(
-        <div >
-            <About/>
-            <Carousels 
-            places= {places}/>
-        </div>
-    )
+  function loadPlaces() {
+    API.getLocation()
+      .then((res) => setPlaces(res.data))
+      .catch((err) => console.log(err));
+  }
+  return (
+    <div className="container home-container">
+      <div className="row">
+        <About />
+      </div>
+
+      <Carousels places={places} />
+    </div>
+  );
 }
 
 export default Home;
