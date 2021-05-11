1b90712b4bb9afae93f86fda1ebecf258c887373
ejsmith13 <ejsmithwork@gmail.com>
2021-05-10 20:17:26 -0400

    collapsable navBar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,41 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import AuthenticationButton from "../AuthenticationBtn";
 
 const NavBar = () => {
+  const [showCollapsedMenu, setShowCollapsedMenu] = useState(false);
+  const show = showCollapsedMenu ? " show" : "";
+
+  function toggleMenu() {
+    setShowCollapsedMenu(!showCollapsedMenu);
+  }
   return (
     <div>
-      <nav className="navbar navbar-dark bg-dark">
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark">
         <span className="navbar-brand mb-0 h1">RCity</span>
-        
-        <ul className="nav justify-content-end">
-       
-          <li className="nav-item">
-            <a className="nav-link active text-white" href="/home">
-              Home
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link active text-white" href="/map">
-              Map view
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link active text-white" href="/list">
-              List view
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link active text-white" href="/add">
-              Add Location
-            </a>
-          </li>
-          <li className="nav-item">
-          <AuthenticationButton />
-          </li>
+        <button
+          className="navbar-toggler"
+          onClick={toggleMenu}
           
-        </ul>
-
-        
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span onClick={console.log(showCollapsedMenu)} className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className={"collapse navbar-collapse justify-content-end" + show}
+          id="navbarSupportedContent"
+        >
+          <ul className="nav ">
+            <li className="nav-item">
+              <a className="nav-link active text-white" href="/home">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active text-white" href="/map">
+                Map view
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active text-white" href="/list">
+                List view
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active text-white" href="/add">
+                Add Location
+              </a>
+            </li>
+            <li className="nav-item">
+              <AuthenticationButton />
+            </li>
+          </ul>
+        </div>
       </nav>
     </div>
   );
