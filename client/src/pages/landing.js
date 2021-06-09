225c5c220af43d543a3265a704167d2a313a3659
ejsmith13 <ejsmithwork@gmail.com>
2021-06-08 20:07:43 -0400

    cleaning up unused code

--- client/src/pages/landing.js
+++ client/src/pages/landing.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import LoginButton from '../components/LoginBtn'
-import SignupButton from '../components/SignUpBtn'
-import LogoutButton from '../components/LogoutBtn'
-import AuthenticationButton from '../components/AuthenticationBtn'
+
 
 
 const Landing =()=> {
